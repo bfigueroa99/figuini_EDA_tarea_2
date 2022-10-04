4e5049f54e8e566e0e79fff366823b5877b9bf91
bfigueroa99 <bfigueroa@miuandes.cl>
2022-10-04 20:23:32 -0300

    read html comentarios

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main(){
-    ifstream archivo("/home/figue/Documents/Codigos/EDA/Tareas/tarea2/figuini_EDA_tarea_2/test.html");
+    ifstream archivo("/home/figue/Documents/Codigos/EDA/Tareas/tarea2/figuini_EDA_tarea_2/test.html"); //hay que editar esto dependiendo el caso, pero en mi caso es asi.
     string texto;
 
     if(archivo.fail()){

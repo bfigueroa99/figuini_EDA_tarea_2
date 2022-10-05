744e6b86f00f81aae7a6a54e2ace3649229d2f79
bfigueroa99 <bfigueroa@miuandes.cl>
2022-10-05 15:09:15 -0300

    add if si el estring esta dentro de la linea

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 #include<fstream>
 #include<cstdlib>  
+#include<algorithm>
 
 using namespace std;
 
 int main(){
     ifstream archivo("/home/figue/Documents/Codigos/EDA/Tareas/tarea2/figuini_EDA_tarea_2/test.html"); //hay que editar esto dependiendo el caso, pero en mi caso es asi.
     string texto;
+
+    string palabras_reservadas[6] = {"<body>","<center>","<h1>","<p>","<ol>","<li>"};
 
     if(archivo.fail()){
         cout<<"no se pudo abrir"<<endl;
@@ -16,6 +19,29 @@ int main(){
     while(!archivo.eof()){
         getline(archivo,texto);
         cout<<texto<<endl;
+
+        //en las siguientes lineas he hecho un codigo que encuentra las palabras reservadas en cada linea, 
+        // ahora hay que ver si ocupamos stack  como en el ejercicio de los parentesis que subi 
+        // o si intentamos hacer otra cosa.
+        if (texto.find(palabras_reservadas[0]) != std::string::npos) {
+            std::cout << "found!" << '\n';
+        }
+        if (texto.find(palabras_reservadas[1]) != std::string::npos) {
+            std::cout << "found!" << '\n';
+        }
+        if (texto.find(palabras_reservadas[2]) != std::string::npos) {
+            std::cout << "found!" << '\n';
+        }
+        if (texto.find(palabras_reservadas[3]) != std::string::npos) {
+            std::cout << "found!" << '\n';
+        }
+        if (texto.find(palabras_reservadas[4]) != std::string::npos) {
+            std::cout << "found!" << '\n';
+        }
+        if (texto.find(palabras_reservadas[5]) != std::string::npos) {
+            std::cout << "found!" << '\n';
+        }
+        
     }
 
     archivo.close();

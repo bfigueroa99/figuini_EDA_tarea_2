ab11760cf19f6c17381b366f02fa53232277b947
bfigueroa99 <bfigueroa@miuandes.cl>
2022-10-05 15:15:51 -0300

    fix array palabras_reservadas

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(){
     ifstream archivo("/home/figue/Documents/Codigos/EDA/Tareas/tarea2/figuini_EDA_tarea_2/test.html"); //hay que editar esto dependiendo el caso, pero en mi caso es asi.
     string texto;
 
-    string palabras_reservadas[6] = {"<body>","<center>","<h1>","<p>","<ol>","<li>"};
+    string palabras_reservadas[12] = {"<body>","</body>","<center>","</center>","<h1>","</h1>","<p>","</p>","<ol>","</ol>","<li>","</li>"};
 
     if(archivo.fail()){
         cout<<"no se pudo abrir"<<endl;
@@ -23,6 +23,8 @@ int main(){
         //en las siguientes lineas he hecho un codigo que encuentra las palabras reservadas en cada linea, 
         // ahora hay que ver si ocupamos stack  como en el ejercicio de los parentesis que subi 
         // o si intentamos hacer otra cosa.
+
+        //posiblemante haremos un for o algo aqui, jajajajaja
         if (texto.find(palabras_reservadas[0]) != std::string::npos) {
             std::cout << "found!" << '\n';
         }

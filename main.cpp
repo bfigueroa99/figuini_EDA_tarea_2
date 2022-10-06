5f3c9efa794d309b33c1ba932694a00790ee2655
bfigueroa99 <bfigueroa@miuandes.cl>
2022-10-05 22:26:19 -0300

    adaptando codigo del profe,en proceso

--- main.cpp
+++ main.cpp
@@ -64,40 +64,48 @@ int main(int argc, char* argv[]){
     //este freopen sera para escribir en el main.log
     freopen("/home/figue/Documents/Codigos/EDA/Tareas/tarea2/figuini_EDA_tarea_2/main.log", "w", stdout); //hay que editar esto para que sea en tu caso
     
+    
     if(archivo.fail()){
         cout<<"no se pudo abrir"<<endl;
         exit(1);
     }
 
+    int pos=0;
+    int i = 0;
+    bool error = false;
+    eda::Stack stack;
+
     while(!archivo.eof()){
         getline(archivo,texto);
         cout<<texto<<endl;
 
-        //en las siguientes lineas he hecho un codigo que encuentra las palabras reservadas en cada linea, 
-        // ahora hay que ver si ocupamos stack  como en el ejercicio de los parentesis que subi 
-        // o si intentamos hacer otra cosa.
-
-        //posiblemante haremos un for o algo aqui, jajajajaja
-        if (texto.find(palabras_reservadas[0]) != std::string::npos) {
-            std::cout << "found!" << '\n';
-        }
-        if (texto.find(palabras_reservadas[1]) != std::string::npos) {
-            std::cout << "found!" << '\n';
-        }
-        if (texto.find(palabras_reservadas[2]) != std::string::npos) {
-            std::cout << "found!" << '\n';
-        }
-        if (texto.find(palabras_reservadas[3]) != std::string::npos) {
-            std::cout << "found!" << '\n';
-        }
-        if (texto.find(palabras_reservadas[4]) != std::string::npos) {
-            std::cout << "found!" << '\n';
-        }
-        if (texto.find(palabras_reservadas[5]) != std::string::npos) {
-            std::cout << "found!" << '\n';
-        }
+		if (texto.find(palabras_reservadas[0]) != std::string::npos) {
+			stack.push(new eda::Node(texto[i]));
+		}
+		if (texto.find(palabras_reservadas[1]) != std::string::npos) {
+			if (stack.isEmpty()){
+				error = true;
+			}
+			else{
+				stack.pop();
+			}
+		}
+		i = i + 1;
         
     }
+
+    if (!stack.isEmpty()){
+		error = true;
+	}
+	pos = i;
+
+    if (!error){
+		std::cout<< " Expresión Correcta " << std::endl;
+	}
+	else{
+		std::cout<< " Expresión Inválida" << std::endl;
+		std::cout<< "Pos error:  " << pos	<< std::endl;
+	}
 
     archivo.close();
     std::fclose(stdout);
